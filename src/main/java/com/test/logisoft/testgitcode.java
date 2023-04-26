a44ca3c81a51d70fa9a0eac2931299397e867715
rmnelapatla70 <rama_70@yahoo.com>
2023-04-26 07:20:33 +0530

    add new method  addnumber

--- src/main/java/com/test/logisoft/testgitcode.java
+++ src/main/java/com/test/logisoft/testgitcode.java
@@ -1,6 +1,10 @@
 package com.test.logisoft;
 
 public class testgitcode {
+
+  public void addnumber( int a , int b){
+    System.out.println(a+b);
+  }
   
   public static void main(String arg[]) {
     int a = 0;
