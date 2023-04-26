c0113cd2260cb9fa6ec3f5f277f666558bc34e29
rmnelapatla70 <rama_70@yahoo.com>
2023-04-26 07:17:58 +0530

    changes code error

--- src/main/java/com/test/logisoft/testgitcode.java
+++ src/main/java/com/test/logisoft/testgitcode.java
@@ -2,6 +2,7 @@ package com.test.logisoft;
 
 public class testgitcode {
   
-  public static void main(String arg[]){
-    int a=0;
+  public static void main(String arg[]) {
+    int a = 0;
+  }
 }
