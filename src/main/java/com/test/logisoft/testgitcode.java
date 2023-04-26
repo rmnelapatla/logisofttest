c20949e8d4b7c4dd73e3729c2d918582a78a88c5
ramana <rmnelapatla@users.noreply.github.com>
2023-04-26 07:22:18 +0530

    Update testgitcode.java

--- src/main/java/com/test/logisoft/testgitcode.java
+++ src/main/java/com/test/logisoft/testgitcode.java
@@ -3,12 +3,12 @@ package com.test.logisoft;
 public class testgitcode {
 
   public void addnumber( int a , int b){
-    System.out.println(a+b);
+    System.out.println(a%b);
   }
   
-  int age=20;
+  int age=120;
   
   public static void main(String arg[]) {
-    int a = 0;
+    int a = 100;
   }
 }
